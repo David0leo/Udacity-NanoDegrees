577780254de19793cebaf8cd146fb7b5675b7b43
David0leo <dla2@ualberta.ca>
2017-08-16 01:04:14 -0600

    Add basic code for CategoryView. Category view takes in posts that are assumed to be of a certain category. Then renders PostCard components for each post passed as a prop.

--- react/readable/src/components/CategoryView.js
+++ react/readable/src/components/CategoryView.js
@@ -1,3 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Modal from 'react-modal'
+import PostCard from './post-card/PostCard'
+
+const CategoryView = ({posts}) => {
+  return (
+    <div>
+      {posts.map((post) =>
+      <PostCard 
+        key={post.id}
+        post={post}
+      />  
+      )}
+    </div>
+  )
+}
+
+export default CateoryView
