cc360ce87afd8325a751595113ba6f190efae2e6
David0leo <dla2@ualberta.ca>
2017-08-16 00:42:12 -0600

    Update the way default data is handled. It is pretty much the same functionality, but it allows for default values for all expected values. That is, if our data has any holes, the holes get patched up with default data.

--- react/readable/src/components/post-card/PostCard.js
+++ react/readable/src/components/post-card/PostCard.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { MdThumbUp, MdThumbDown } from 'react-icons/lib/md'
 import './PostCard.css'
 import postUnixTimeToSimplifiedTimeElapsedString from '../../utils/helpers'
 
 const PostCard = ({ post }) => {
-  if (!post) {
-    post = {
-      id: '123456',
-      timestamp: Date.now(),
-      title: 'Test Title',
-      body: "This is a test of what a post body preview might look like. You would have a large amount of text, but you wouldn't be able to see all of it because it is only a preview. Ideally, it would look like the first portion of the text followed by elipses.",
-      author: 'John Snow',
-      category: 'Game of Thrones',
-      voteScore: 0,
-      deleted: false
-    }
+  const defaultPost = {
+    id: '123456',
+    timestamp: Date.now(),
+    title: 'Test Title',
+    body: "This is a test of what a post body preview might look like. You would have a large amount of text, but you wouldn't be able to see all of it because it is only a preview. Ideally, it would look like the first portion of the text followed by elipses.",
+    author: 'John Snow',
+    category: 'Game of Thrones',
+    voteScore: 0,
+    deleted: false
+  }
+  // if no post info is passed in, it will use the default post values
+  // current values are just for testing purposes
+  post = {
+    ...defaultPost,
+    ...post
   }
   return (
     <div className="post-card">
