3a241fea136219efed6d3c024c8d3fc67d07cb9b
David0leo <dla2@ualberta.ca>
2017-08-23 13:42:54 -0600

    Do some cool stuff with icons mostly. Add some style bits and get a lot of the structural stuff in.

--- react/readable/src/components/ReadableCategoryNavPane.js
+++ react/readable/src/components/ReadableCategoryNavPane.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../style/css/ReadableAppStyle.css'
+import { MdMenu } from 'react-icons/lib/md'
 
 const ReadableCategoryNavPane = ({ categories }) => {
   return (
     <div className="readable-category-nav-pane">
-      <button className="nav-pane-button"></button>
+      <button className="nav-pane-button">
+        <MdMenu size={30}></MdMenu>
+      </button>
       <h3 className="nav-pane-header">Categories</h3>
       <ol className="nav-pane-ordered-list">
         {categories.map((category) => 
