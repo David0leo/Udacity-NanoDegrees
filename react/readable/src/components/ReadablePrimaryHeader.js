2e543ae2dc082820b7c4bd00d73a459dd4d7c8aa
David0leo <dla2@ualberta.ca>
2017-08-27 13:05:25 -0600

    Create a new favicon.ico just to add a nice cohesive feel.

--- react/readable/src/components/ReadablePrimaryHeader.js
+++ react/readable/src/components/ReadablePrimaryHeader.js
@@ -5,7 +5,11 @@ import ReadableIcon from './ReadableIcon'
 export default ReadablePrimaryHeader = ({ category='all' }) => {
   return (
     <div className="readable-main-title">
-      <ReadableIcon size={100} primaryColor={"white"} secondaryColor={"currentColor"}></ReadableIcon>
+      <ReadableIcon 
+        size={100} 
+        primaryColor={"white"} 
+        secondaryColor={"currentColor"}>
+      </ReadableIcon>
       <h1 className="app-title">Readable</h1>
       <h2 className="app-category">/{category}</h2>
     </div>
